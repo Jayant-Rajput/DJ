89289a079f2a72ef546c1a05dd159bb795311cd5
dipteshrastogi <dipteshraj2003@gmail.com>
2025-05-16 16:33:33 +0530

    exp with noti

--- frontend/src/components/ContestCards.jsx
+++ frontend/src/components/ContestCards.jsx
@@ -33,7 +33,7 @@ const ContestCards = ({ contest, isBookmarked }) => {
 
         console.log("Days", days, "hours", hours, "minutes", minutes, "seconds", seconds);
 
-        if(days === 1 && hours === 3 && (minutes === 32 && seconds <=20 && seconds>=10) ){
+        if(days === 1 && hours === 3 && (minutes === 30 && seconds <=20 && seconds>=15) ){
           console.log("Inside if condition");
           sendNoti({title: contest.title, rawStartTime:  contest.rawStartTime});
         }
